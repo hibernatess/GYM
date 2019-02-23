6376751871e868c2f3b22802a633d9ee5ae847f5
hibernatess <1959227206@qq.com>
2019-02-23 13:11:47 +0800

    tijiao

---
+++ src/main/java/com/gym/ssm/base/Test.java
@@ -0,0 +1,13 @@
+package com.gym.ssm.base;
+
+/**
+ * @ClassName Test
+ * @Author ywj
+ * @Describe
+ * @Date 2019/2/23 0023 13:10
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
