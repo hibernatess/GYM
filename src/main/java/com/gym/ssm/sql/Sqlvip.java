09df33c623f53517b52a6257f5769d924fea96b2
ThatAdult <3163456361@qq.com>
2019-02-27 21:18:53 +0800

    ss

---
+++ src/main/java/com/gym/ssm/sql/Sqlvip.java
@@ -0,0 +1,31 @@
+package com.gym.ssm.sql;
+
+import com.gym.ssm.entity.Coach;
+import com.gym.ssm.entity.vip;
+import org.apache.ibatis.jdbc.SQL;
+import org.springframework.util.StringUtils;
+
+public class Sqlvip {
+    public String uploadimg(vip vip){
+        String sql = new SQL() {
+            {
+                UPDATE("vip" );
+                SET("img='"+vip.getImg()+"'");
+                WHERE ("hid="+vip.getHid()+"");
+            }
+        }.toString();
+        return  sql ;
+    }
+
+
+    public String update(vip vip){
+        String sql = new SQL() {
+            {
+                UPDATE("vip" );
+                SET("hpwd='"+vip.getHpwd()+"',hphone='"+vip.getHphone()+"',hsex='"+vip.getHsex()+"',hage="+vip.getHage()+",haddress='"+vip.getHaddress()+"',remark='"+vip.getRemark()+"'");
+                WHERE ("hid="+vip.getHid()+"");
+            }
+        }.toString();
+        return  sql ;
+    }
+}
