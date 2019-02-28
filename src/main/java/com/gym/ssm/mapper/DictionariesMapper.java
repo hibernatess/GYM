0f296661079f897e4033a79856f074e53349fdde
hibernatess <1959227206@qq.com>
2019-02-28 14:19:53 +0800

    ywj

--- src/main/java/com/gym/ssm/mapper/DictionariesMapper.java
+++ src/main/java/com/gym/ssm/mapper/DictionariesMapper.java
@@ -11,4 +11,7 @@ import java.util.Map;
 public interface DictionariesMapper extends Mapper<Dictionaries> {
      @SelectProvider(type = SqlDictionaries.class ,method = "SelectDictionaries")
     public List<Dictionaries>   GetDictionaries(Dictionaries dictionaries);
+
+    @SelectProvider(type = SqlDictionaries.class ,method = "getSname")
+     public List<Map<String,Object>> getSname();
 }
